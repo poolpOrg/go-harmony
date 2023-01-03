6ae36b17126d91a52bc26d98c07dbd7a8bb09550
Gilles Chehade <gilles@poolp.org>
2023-01-03 13:50:11 +0100

    increase test coverage

--- instruments/instruments_test.go
+++ instruments/instruments_test.go
@@ -2,18 +2,50 @@ package instruments
 
 import (
 	"testing"
+
+	"github.com/poolpOrg/go-harmony/naturals"
+	"github.com/poolpOrg/go-harmony/octaves"
+	"github.com/poolpOrg/go-harmony/tunings"
 )
 
 func TestInstruments(t *testing.T) {
 }
 
 func TestInstruments_NewInstrument(t *testing.T) {
+	tuningsList := tunings.Tunings()
+	for _, tuning := range tuningsList {
+		_ = NewInstrument(tuning)
+	}
 }
 
 func TestInstruments_Natural(t *testing.T) {
+	instrument := NewInstrument(tunings.A440)
+
+	naturalsList := naturals.Naturals()
+	for _, natural := range naturalsList {
+		got, err := instrument.Natural(natural.Name())
+		if err != nil {
+			t.Fatalf(`instrument.Natural(%s) = error(%s)`, natural.Name(), err)
+		}
+		if got.Name() != natural.Name() {
+			t.Fatalf(`instrument.Natural(%s) = %s, want %s`, natural.Name(), got.Name(), natural.Name())
+		}
+	}
 }
 
 func TestInstruments_Octave(t *testing.T) {
+	instrument := NewInstrument(tunings.A440)
+
+	octavesList := octaves.Octaves()
+	for _, octave := range octavesList {
+		got, err := instrument.Octave(octave.Name())
+		if err != nil {
+			t.Fatalf(`instrument.Octave(%s) = error(%s)`, octave.Name(), err)
+		}
+		if got.Name() != octave.Name() {
+			t.Fatalf(`instrument.Octave(%s) = %s, want %s`, octave.Name(), got.Name(), octave.Name())
+		}
+	}
 }
 
 func TestInstruments_Note(t *testing.T) {
